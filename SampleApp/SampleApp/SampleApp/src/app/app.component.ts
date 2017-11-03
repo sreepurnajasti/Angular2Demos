3d14ae1c593bdc5d2bf382c0b00233dfc7cdf13d
sreepurnajasti <sreepurna.jasti@gmail.com>
2017-11-03 11:02:43 +0530

    Starter files to get started with angular2 application

---
+++ SampleApp/SampleApp/SampleApp/src/app/app.component.ts
@@ -0,0 +1,7 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `Angular Js2 App`
+})
+export class AppComponent { }
---
+++ SampleApp/SampleApp/SampleApp/src/app/app.module.ts
@@ -0,0 +1,17 @@
+﻿import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
+    declarations: [
+        AppComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
